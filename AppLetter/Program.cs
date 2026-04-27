a7e13b35ee70f89eb55e91fa4faabe3fb4eeb68b
Reflex414 <9629970659rus@mail.ru>
2026-04-27 11:48:23 +0300

    Добавил xml комментарии

--- AppLetter/Program.cs
+++ AppLetter/Program.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 
-// Выполнил все действия по руководству
+/// <summary>
+/// Класс для демонстрации работы с массивами и строками.
+/// </summary>
 class ArrayExample
 {
+    /// <summary>
+    /// Главный метод программы. Формирует строку из символов и вызывает метод вывода сообщений.
+    /// </summary>
     static void Main()
     {
         char[] letters = { 'f', 'r', 'e', 'd', ' ', 's', 'm', 'i', 't', 'h' };
         string name = "";
         int[] a = new int[10];
+
         for (int i = 0; i < letters.Length; i++)
         {
             name += letters[i];
@@ -17,6 +23,13 @@ class ArrayExample
         Console.ReadKey();
     }
 
+    /// <summary>
+    /// Выводит персонализированное сообщение с счетчиком в консоль.
+    /// </summary>
+    /// <param name="name">Имя, собранное из массива символов.
+    /// </param>
+    /// <param name="msg">Числовое значение (счетчик).
+    /// </param>
     static void SendMessage(string name, int msg)
     {
         Console.WriteLine("Hello, " + name + "! Count to " + msg);
