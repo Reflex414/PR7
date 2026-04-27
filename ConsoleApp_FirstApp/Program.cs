44dd599e3a441eb44d29b38a78ae4d4e6f43c875
Reflex414 <9629970659rus@mail.ru>
2026-04-27 11:45:54 +0300

    Добавил xml комментарии

--- ConsoleApp_FirstApp/Program.cs
+++ ConsoleApp_FirstApp/Program.cs
@@ -3,8 +3,14 @@ using System.Collections.Generic;
 
 namespace ConsoleApp_FirstApp
 {
+    /// <summary>
+    /// Главный класс приложения.
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Точка входа в программу.
+        /// </summary>
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Galaxy News!");
@@ -12,17 +18,20 @@ namespace ConsoleApp_FirstApp
             Console.ReadKey();
         }
 
+        /// <summary>
+        /// Создает список галактик и выводит информацию о них в консоль.
+        /// </summary>
         private static void IterateThroughList()
         {
             var theGalaxies = new List<Galaxy>
-        {
-            new Galaxy() { Name="Tadpole", MegaLightYears=400, GalaxyType=new GType('S')},
-            new Galaxy() { Name="Pinwheel", MegaLightYears=25, GalaxyType=new GType('S')},
-            new Galaxy() { Name="Cartwheel", MegaLightYears=500, GalaxyType=new GType('L')},
-            new Galaxy() { Name="Small Magellanic Cloud", MegaLightYears=.2, GalaxyType=new GType('I')},
-            new Galaxy() { Name="Andromeda", MegaLightYears=3, GalaxyType=new GType('S')},
-            new Galaxy() { Name="Maffei 1", MegaLightYears=11, GalaxyType=new GType('E')}
-        };
+            {
+                new Galaxy() { Name="Tadpole", MegaLightYears=400, GalaxyType=new GType('S')},
+                new Galaxy() { Name="Pinwheel", MegaLightYears=25, GalaxyType=new GType('S')},
+                new Galaxy() { Name="Cartwheel", MegaLightYears=500, GalaxyType=new GType('L')},
+                new Galaxy() { Name="Small Magellanic Cloud", MegaLightYears=.2, GalaxyType=new GType('I')},
+                new Galaxy() { Name="Andromeda", MegaLightYears=3, GalaxyType=new GType('S')},
+                new Galaxy() { Name="Maffei 1", MegaLightYears=11, GalaxyType=new GType('E')}
+            };
 
             foreach (Galaxy theGalaxy in theGalaxies)
             {
@@ -39,17 +48,32 @@ namespace ConsoleApp_FirstApp
         }
     }
 
+    /// <summary>
+    /// Модель данных, представляющая галактику.
+    /// </summary>
     public class Galaxy
     {
+        /// <summary>Название галактики.</summary>
         public string Name { get; set; }
 
+        /// <summary> Расстояние до галактики в миллионах световых лет.
+        /// </summary>
         public double MegaLightYears { get; set; }
-        public GType GalaxyType { get; set; }
 
+        /// <summary>Классификация типа галактики.</summary>
+        public GType GalaxyType { get; set; }
     }
 
+    /// <summary>
+    /// Класс для определения и хранения типа галактики.
+    /// </summary>
     public class GType
     {
+        /// <summary>
+        /// Инициализирует новый экземпляр класса GType.
+        /// </summary>
+        /// <param name="type"> Символ, обозначающий тип (S, E, I, L).
+        /// </param>
         public GType(char type)
         {
             switch (type)
@@ -70,7 +94,13 @@ namespace ConsoleApp_FirstApp
                     break;
             }
         }
+
+        /// <summary> Установленный тип галактики из перечисления.
+        /// </summary>
         public object MyGType { get; set; }
+
+        /// <summary> Внутреннее перечисление доступных типов галактик.
+        /// </summary>
         private enum Type { Spiral, Elliptical, Irregular, Lenticular }
     }
 }
